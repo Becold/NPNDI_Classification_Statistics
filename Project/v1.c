adf8ee1996b0ba52ca55a3c31bcb2dbc447a0d3b
Romain Marbaix <romain.marbaix@oniryx.eu>
2020-03-13 23:47:14 +0100

    Add specifications of methods (v1:without types/v2:with types)

---
+++ Project/v1.c
@@ -0,0 +1,33 @@
+/*
+
+module(displayResultsForEachClasses;realClasses, estimatedClasses;)
+
+module(displayAccuracy;realClasses, estimatedClasses;)
+
+module(displayBarCharts;realClasses, estimatedClasses;)
+
+
+
+module(researchClasses;realClasses, estimatedClasses, size;classes, nbClasses, sumNbGoodEstimation, sumTotal, maxTotal)
+
+module(displayLegende;;)
+
+module(displayXaxis;maxValue, ratio;)
+
+module(displayBar;value, ratio;)
+
+module(addOrUpdateClasses;classes, real, estimated;classes, nbClasses, sumNbGoodEstimation, sumTotal, maxTotal)
+
+module(updateClasse;pClasse, isCorrect;)
+
+module(addClasse;real, isCorrectEstimation;classes, pPrecClasse, nbClasses, maxTotal)
+
+module(createClasse;name, isCorrectEstimation;classe)
+
+module(updateMaxTotal;total;maxTotal)
+
+module(updateSumTotalAndSumNbGoodEstimation;isCorrectEstimation;sumTotal, sumNbGoodEstimation)
+
+module(calculatePourcentage;numerator, denominator;)
+
+*/
